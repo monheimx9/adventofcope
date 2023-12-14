37d5e92b9a4934a2837473851a210c6b1be84c6a
Jeremy Delley <jeremy.delley@imex-dtp.com>
2023-12-14 22:09:45 +0100

    i might be too stupid

--- 2023/day13/src/main.rs
+++ 2023/day13/src/main.rs
@@ -29,29 +29,109 @@ impl Mountain {
         Mountain { cluster }
     }
     fn find_mirror(&self) -> usize {
-        if let Some(i) = self
-            .cluster
-            .windows(2)
-            .enumerate()
-            .find(|(_, a)| a[0] == a[1])
-            .map(|(i, _)| i)
-        {
-            if i > 0 && (self.cluster[i - 1] == self.cluster[i + 1]) {
-                return (i + 1) * 100;
+        fn find_hori(m: &Mountain, p: usize) -> usize {
+            if p >= m.cluster.len() - 2 {
+                return 0;
             }
+            let mut is_broken: bool = false;
+            let index: usize = p;
+            if let Some(i) = m
+                .cluster
+                .windows(2)
+                .skip(p)
+                .enumerate()
+                .find(|(_, a)| a[0] == a[1])
+                .map(|(i, _)| i)
+            {
+                if is_greater(i, m.cluster.len()) {
+                    for n in 0..m.cluster.len() - i {
+                        if m.cluster[i + 1 - n] != m.cluster[i + n] {
+                            is_broken = true;
+                            break;
+                        }
+                    }
+                } else {
+                    for n in 0..i {
+                        if m.cluster[i - n] != m.cluster[i + n] {
+                            is_broken = true;
+
+                            find_hori(m, index + 1);
+                            break;
+                        }
+                    }
+                }
+                if !is_broken {
+                    return (i + 1) * 100;
+                }
+            }
+            0
         }
 
-        for n in 0..self.cluster[0].len() - 1 {
-            let a = &self.cluster[n][0..];
-            let b = &self.cluster[n + 1][0..];
-
-            if a == b {
-                return n + 1;
+        fn find_verti(m: &Mountain, p: usize) -> usize {
+            if p >= m.cluster[0].len() - 2 {
+                return 0;
             }
+            let mut is_broken = false;
+            let index: usize = p;
+            for n in 0..m.cluster[0].len() - 1 {
+                let a = collect_column(&m.cluster, n);
+                let b = collect_column(&m.cluster, n + 1);
+
+                if a == b {
+                    if is_greater(n, m.cluster[0].len()) {
+                        for o in 0..m.cluster[0].len() - n {
+                            let a = collect_column(&m.cluster, n + 1 - o);
+                            let b = collect_column(&m.cluster, o + n);
+
+                            if a != b {
+                                is_broken = true;
+                                break;
+                            }
+                        }
+                    } else {
+                        for o in 0..n {
+                            let a = collect_column(&m.cluster, o);
+                            let b = collect_column(&m.cluster, o + 1);
+
+                            if a != b {
+                                is_broken = true;
+                                find_verti(m, index + 1);
+                                break;
+                            }
+                        }
+                    }
+                    if !is_broken {
+                        return n + 1;
+                    }
+                }
+            }
+            0
         }
 
-        0
+        let o = find_hori(self, 0) + find_verti(self, 0);
+        println!("{o}");
+        o
     }
+}
+
+fn is_greater(i: usize, l: usize) -> bool {
+    i >= l / 2
+}
+
+fn collect_column<T>(grid: &[Vec<T>], column_index: usize) -> Option<Vec<&T>> {
+    if let Some(row) = grid.get(0) {
+        if column_index >= row.len() {
+            return None;
+        }
+    } else {
+        return None;
+    }
+
+    let column: Vec<&T> = grid
+        .iter()
+        .filter_map(|row| row.get(column_index))
+        .collect();
+    Some(column)
 }
 
 struct LavaIsland {
@@ -68,6 +148,10 @@ impl LavaIsland {
 
 fn main() {
     println!("Hello, world!");
+    let s = include_str!("../input.txt");
+    let m = LavaIsland::from_str(s);
+    let i: usize = m.mountains.iter().map(|c| c.find_mirror()).sum();
+    println!("Part one mirrors = {i}");
 }
 
 #[cfg(test)]
