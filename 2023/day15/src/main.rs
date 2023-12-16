f227dd3508f1a9d21af5b22210469aa5b31e5362
Jeremy Delley <jeremy.delley@imex-dtp.com>
2023-12-16 10:55:00 +0100

    completed 2023.15

--- 2023/day15/src/main.rs
+++ 2023/day15/src/main.rs
@@ -1,13 +1,25 @@
-use std::collections::{BTreeMap, HashMap};
-use std::hash::Hash;
+use std::collections::BTreeMap;
+use std::time::Instant;
 use std::usize;
 
-struct LensBox(HashMap<usize, LensLabel>);
+struct LensBox(BTreeMap<usize, LensLabel>);
 
 struct LensLabel(Vec<(String, usize)>);
 
 fn main() {
     println!("Hello, world!");
+    let s = include_str!("../input.txt")
+        .trim()
+        .split(',')
+        .collect::<Vec<_>>();
+    let ins = Instant::now();
+    let o = solve_hash(&s);
+    println!("Part one is {}", o);
+    println!("In: {} micros", ins.elapsed().as_micros());
+    let ins = Instant::now();
+    let o = solve_box(&s);
+    println!("Part two is {}", o);
+    println!("In: {} micros", ins.elapsed().as_micros());
 }
 
 fn solve_hash(sl: &[&str]) -> usize {
@@ -28,7 +40,7 @@ fn hash_label(s: &str) -> usize {
 }
 
 fn solve_box(sl: &[&str]) -> usize {
-    let mut lbox: LensBox = LensBox(HashMap::new());
+    let mut lbox: LensBox = LensBox(BTreeMap::new());
 
     for s in sl.iter() {
         if let Some((h, l)) = s.split_once('=') {
@@ -36,7 +48,21 @@ fn solve_box(sl: &[&str]) -> usize {
             let le: usize = l.parse().unwrap();
             perform_stuff_in_box(&mut lbox, &Ope::Replace, h, ha, le);
         }
+        if let Some((h, _)) = s.split_once('-') {
+            let ha = hash_label(h);
+            perform_stuff_in_box(&mut lbox, &Ope::Remove, h, ha, 0);
+        }
     }
+    lbox.0
+        .iter()
+        .enumerate()
+        .map(|(_, (i, b))| {
+            b.0.iter()
+                .enumerate()
+                .map(|(i2, (_, l))| (i + 1) * (i2 + 1) * l)
+                .sum::<usize>()
+        })
+        .sum()
 }
 
 enum Ope {
@@ -46,8 +72,33 @@ enum Ope {
 
 fn perform_stuff_in_box(b: &mut LensBox, op: &Ope, labstr: &str, labhash: usize, lens: usize) {
     match op {
-        Ope::Replace => {}
-        Ope::Remove => {}
+        Ope::Replace => {
+            let bo = b.0.entry(labhash).or_insert(LensLabel(Vec::new()));
+            if bo.0.is_empty() {
+                bo.0.push((labstr.to_string(), lens));
+            } else {
+                let mut found = false;
+                for l in 0..bo.0.len() {
+                    if bo.0[l].0 == labstr {
+                        bo.0[l].1 = lens;
+                        found = true;
+                    }
+                }
+                if !found {
+                    bo.0.push((labstr.to_string(), lens));
+                }
+            }
+        }
+        Ope::Remove => {
+            if let Some(bo) = b.0.get_mut(&labhash) {
+                for l in 0..bo.0.len() {
+                    if bo.0[l].0 == labstr {
+                        bo.0.remove(l);
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -86,5 +137,23 @@ mod tests {
             .collect::<Vec<_>>();
         let o = solve_hash(&s);
         assert_eq!(o, 513158)
+    }
+    #[test]
+    fn pt2_1() {
+        let s = include_str!("../test.txt")
+            .trim()
+            .split(',')
+            .collect::<Vec<_>>();
+        let o = solve_box(&s);
+        assert_eq!(o, 145)
+    }
+    #[test]
+    fn pt2() {
+        let s = include_str!("../input.txt")
+            .trim()
+            .split(',')
+            .collect::<Vec<_>>();
+        let o = solve_box(&s);
+        assert_eq!(o, 200277)
     }
 }
