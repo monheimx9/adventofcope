56e669fc77b676d34623a23daca8495f5c9efb82
Jeremy Delley <jeremy.delley@imex-dtp.com>
2023-12-19 21:34:37 +0100

    mmmmmm

--- 2023/day17/src/main.rs
+++ 2023/day17/src/main.rs
@@ -7,8 +7,6 @@ struct Node {
     x: usize,
     y: usize,
     previous: Option<usize>,
-    max_x: Option<usize>,
-    max_y: Option<usize>,
 }
 impl Node {
     fn from_str(s: &str) -> Vec<Self> {
@@ -24,34 +22,49 @@ impl Node {
                         x,
                         y,
                         previous: None,
-                        max_x: None,
-                        max_y: None,
                     })
                     .collect::<Vec<Node>>()
             })
             .collect()
     }
 }
-fn solve_1(nodes: &mut [Node], src: &mut usize, dst: usize) -> usize {
-    let mut queue: Vec<(usize, &Node)> = Vec::new();
+fn solve_1(nodes: &mut [Node], src: usize, dst: usize) -> usize {
+    let mut queue: Vec<(usize, Node)> = Vec::new();
+    let mut next_src = src;
     while queue.len() < nodes.len() {
-        let current_node = nodes[*src];
+        let mut temp_nodes: Vec<(usize, usize)> = Vec::new();
+        let previous = src;
+        let current_node = nodes[next_src];
+        let mut count: usize = 0;
         for n in 0..nodes.len() {
             if is_x_valid(&current_node, &nodes[n]) {
                 if !is_at_limit(nodes, n) {
-                    todo!()
+                    let loss = node_loss(&current_node, &nodes[n]);
+                    temp_nodes.push((n, loss));
+                    count += 1;
                 }
             }
             if is_y_valid(&current_node, &nodes[n]) {
                 if !is_at_limit(nodes, n) {
-                    todo!()
+                    let loss = node_loss(&current_node, &nodes[n]);
+                    temp_nodes.push((n, loss));
+                    count += 1;
                 }
             }
+            if count == 4 {
+                break;
+            }
         }
-
-        todo!()
+        let (next_node, loss) = temp_nodes.iter().min_by_key(|(_, value)| value).unwrap();
+        nodes[*next_node].total_loss = Some(*loss);
+        nodes[next_src].previous = Some(previous);
+        queue.push((next_src, nodes[next_src]));
+        next_src = *next_node;
     }
-    todo!()
+    nodes[dst].total_loss.unwrap()
+}
+fn node_loss(src: &Node, dst: &Node) -> usize {
+    src.total_loss.unwrap_or(0) + dst.loss
 }
 
 fn is_at_limit(nodes: &[Node], src: usize) -> bool {
@@ -94,7 +107,9 @@ mod tests {
     #[test]
     fn pt1() {
         let s = include_str!("../test.txt");
-        let nodes = Node::from_str(s);
-        println!("OK")
+        let mut nodes = Node::from_str(s);
+        let dst = nodes.len() - 1;
+        let ans = solve_1(&mut nodes, 0, dst);
+        assert_eq!(ans, 102)
     }
 }
