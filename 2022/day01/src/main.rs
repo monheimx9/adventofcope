ed56746fd956901513e5d3667edac1fb63318053
Jeremy Delley <jeremy.delley@imex-dtp.com>
2024-04-30 23:39:49 +0200

    completed 2022.01
    both stars

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -1,3 +1,53 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
 fn main() {
     println!("Hello, world!");
+    let s = include_str!("../input.txt");
+    assert_eq!(part1(s), 69883);
+    assert_eq!(part2(s), 207576);
+}
+fn part1(s: &str) -> usize {
+    let newstring = s.split_blank();
+    count_max(&newstring)
+}
+fn part2(s: &str) -> usize {
+    let newstring = s.split_blank();
+    top(&newstring)
+}
+
+fn count_max(v: &[&str]) -> usize {
+    v.iter()
+        .map(|l| l.lines().map(|s| s.parse::<usize>().unwrap()).sum())
+        .max()
+        .unwrap()
+}
+
+fn top(v: &[&str]) -> usize {
+    let mut list: Vec<usize> = v
+        .iter()
+        .map(|l| l.lines().map(|s| s.parse::<usize>().unwrap()).sum())
+        .collect();
+    list.sort_by(|a, b| b.cmp(a));
+    list.iter().take(3).sum()
+}
+
+trait SplitBlank {
+    fn split_blank(&self) -> Vec<&str>;
+}
+
+impl SplitBlank for str {
+    fn split_blank(&self) -> Vec<&str> {
+        self.split("\n\n").collect::<Vec<_>>()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn pt1_1() {
+        let teststr = include_str!("../sample.txt");
+        assert_eq!(part1(teststr), 24000);
+    }
 }
